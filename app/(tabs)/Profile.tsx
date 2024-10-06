7a0d7ae7ba7db5059757bdd5ff2ce056b8a84b6c
littlebean47 <jalynb48@gmail.com>
2024-10-06 00:35:05 -0700

    Changed styling to be responsive

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,31 +1,36 @@
-import { Text, View } from "react-native"
+import { Text, View, Dimensions } from "react-native";
 import { Avatar } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
 import { ThemedView } from "@/components/ThemedView";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
+const { width } = Dimensions.get('window'); // Get the device width
 
 const styles = StyleSheet.create({
   avatarContainer: {
     flexDirection: 'row',
     alignItems: 'flex-start',
     justifyContent: 'space-between',
-    padding: 20
+    paddingHorizontal: width * 0.05, // 5% of screen width for padding
+    paddingVertical: 20
   },
   avatar: {
-    margin: 20
+    margin: width * 0.05, // margin based on screen width
   },
   settingsIcon: {
     marginTop: 25,
-    marginRight: 25
+    marginRight: width * 0.05, // margin based on screen width
   }
-})
+});
 
 function Profile() {
   return (
     <ThemedView>
       <View style={styles.avatarContainer}>
-        <Avatar.Image source={require("../../assets/images/gettyimages.jpg")} size={140} style={styles.avatar} />
+        <Avatar.Image source={require("../../assets/images/gettyimages.jpg")}
+          size={width * 0.35}  // 35% of screen width
+          style={styles.avatar}
+        />
         <MaterialCommunityIcons name="cog-outline" size={45} color="#a8a8a8" style={styles.settingsIcon} />
       </View>
       <View>
@@ -36,9 +41,7 @@ function Profile() {
         {/* Upgrade Button */}
       </View>
     </ThemedView>
-  )
+  );
 }
 
-
-
-export default Profile
+export default Profile;

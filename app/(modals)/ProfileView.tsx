fc7882a88cadec83e15ceb34af2a97aa887088f8
littlebean47 <jalynb48@gmail.com>
2024-10-06 12:27:21 -0700

    Added ProfileView Component

---
+++ app/(modals)/ProfileView.tsx
@@ -0,0 +1,12 @@
+import { ThemedView } from "@/components/ThemedView"
+import { Text } from "react-native"
+
+function ProfileView() {
+    return (
+        <ThemedView>
+            <Text>ProfileView Component</Text>
+        </ThemedView>
+    )
+}
+
+export default ProfileView
